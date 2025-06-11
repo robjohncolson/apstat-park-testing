72b61d780d5ae29d33371b8d0691df79ab143951
robert colson <robjohncolson@protonmail.com>
2025-06-10 22:38:40 -0400

    phase 3 module 1 debug

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,9 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  plugins: [],
-}
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+})
