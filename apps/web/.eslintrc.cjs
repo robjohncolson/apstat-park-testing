7158c81307bead6480399ee80ddd4e933350a5ad
robert colson <robjohncolson@protonmail.com>
2025-06-13 19:14:16 -0400

    fix(lint): Correctly configure web workspace to show warnings

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    // Downgrade all previous errors to warnings
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    'prefer-const': 'warn',
+
+    // Keep existing rules, but at 'warn' level
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}; 
